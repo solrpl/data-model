ae2f4364842bd4e5655c6c75badaa79f4714f6ea
Marek Rogoziński <nnegativ@gmail.com>
2012-08-18 16:36:35 +0200

    some tests

--- src/test/java/pl/solr/dm/DataModelTest.java
+++ src/test/java/pl/solr/dm/DataModelTest.java
@@ -2,23 +2,40 @@ package pl.solr.dm;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.StringBufferInputStream;
+
 import org.junit.Test;
 
 import pl.solr.dm.DataModel;
 import pl.solr.dm.producers.JsonDataModelProducer;
 import pl.solr.dm.producers.SolrDataModelProducer;
+import pl.solr.dm.types.ArrayDataType;
 
 public class DataModelTest {
 	
 	@Test
 	public void unserialize() {
 		DataModel model = DataModel.builder().fromJson(DataModelTest.class.getResourceAsStream("/input.json"));
-		for (int i = 0; i < 10; i++) {
+		assertNotNull(model.getValue().getValue().get("id"));
+		assertNotNull(model.getValue().getValue().get("created"));
+		DataType<?> tags = model.getValue().getValue().get("tags");
+		assertNotNull(tags);
+		assertTrue(tags instanceof ArrayDataType);
+		for (int i = 0; i < 3; i++) {
 			System.err.println(new JsonDataModelProducer().convert(model));
 		}
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 3; i++) {
 			System.err.println(new SolrDataModelProducer().convert(model));
 		}
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void unserializeWithError() {
+		ByteArrayInputStream bais = new ByteArrayInputStream("{ syntax_error: }".getBytes());
+		
+		DataModel model = DataModel.builder()
+				.fromJson(bais);
 	}
 
 }
